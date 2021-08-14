cc811524ba87bcab6d9148be0f89904eb2f03289
Shreenanda <shreenandap@gmail.com>
2021-08-14 13:02:01 +0530

    Fetch the data from codeforces api

--- client/src/components/Train.js
+++ client/src/components/Train.js
@@ -1,10 +1,37 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import axios from 'axios'
 const Train = () => {
+    const [handle, setHandle] = useState('')
+    const [generalSet, setAcceptedList]= useState(null)
+    const ChangeInputHandle = (event) => {
+        
+        setHandle(event.target.value)
+    }
+    const handleSubmit =async(event) => {
+        event.preventDefault()
+        console.log("Entered handle is ", handle)
+        try{
+            const response = await axios.get(`https://codeforces.com/api/user.status?handle=${handle}&from=1&count=10`)
+        console.log(response.data.result)
+        const ProblemSetInfo = response.data.result.filter((info) => {
+            return info.verdict === "OK"
+        })
+        setAcceptedList(ProblemSetInfo)
+        
+        }
+        catch (error){
+console.log(error)
+        }
+       
+    }
     return(
         <div>
             <h1>Enter the handle</h1>
-           
+            <form onSubmit = {handleSubmit}>
+            <input value={handle} type = "text" onChange = {ChangeInputHandle}/>
+            <br/>
+            <button type="submit">Add Handle</button>
+            </form>
         </div>
     )
 }
