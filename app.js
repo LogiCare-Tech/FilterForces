76937f340ff01158b5279e1e99fcee649efc9704
Shreenanda <shreenandap@gmail.com>
2021-09-17 15:31:27 +0530

    File structuring

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const morgan = require('morgan')
 
 const cookieParser = require('cookie-parser')
 
+const path = require('path')
+
 const app = express()
 
 const middleware = require('./utils/middleware')
@@ -30,7 +32,13 @@ dotenv.config()
 if (process.env.NODE_ENV === "development") {
   app.use(morgan('dev'))
 }
-
+if(process.env.NODE_ENV === "production")
+{
+  app.use(express.static('client/build'))
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 app.use(express.json())
 
 app.use(cors())
